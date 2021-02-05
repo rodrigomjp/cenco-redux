bbddc8f47f8c77b55ce0ef1afbb6e0f9459ece94
Rodrigo Jorquera <rodrigo@MacBook-Pro-de-Rodrigo.local>
2021-02-05 18:18:28 -0300

    se configura dev tool con middleware

--- src/store/store.js
+++ src/store/store.js
@@ -11,4 +11,7 @@ const reducer = combineReducers({
   auth: authReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
